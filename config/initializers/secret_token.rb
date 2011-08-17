0c93e14794972630a871f2bf1b84f304919be767
lfborjas <luis.borjas@escolarea.com>
2011-08-16 20:22:35 -0400

    just created, with jquery and nifty gens

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PictourMetrics::Application.config.secret_token = '035093f8eed9699ebb2462fdb2940e9b902d878d91f5c1ba4ebd8a042b5abd860ee461eae4a28eb35395519576092cf61861beae097853e09adea41650bcf44c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PictourMetrics::Application.config.session_store :cookie_store, :key => '_pictour_metrics_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PictourMetrics::Application.config.session_store :active_record_store
